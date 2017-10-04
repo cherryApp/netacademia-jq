2a39edd8c0d1e8aae98bcaba25908cc3362332ed
cherryApp <j.cserko@gmail.com>
2017-10-04 18:38:12 +0200

    Feat: animated page load.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ function fadeDone() {
 // Eseménykezelő beállítása.
 $("p").click(function(){
     $(this).hide();
-    $(this).fadeTo(5000, 1, fadeDone);
+    // $(this).fadeTo(5000, 1, fadeDone);
+    $(this).slideDown(3500);
 });
 
 // Esemény kiváltása.
@@ -15,5 +16,10 @@ $("p").click();
 // Kattintás megelőzése.
 $("nav a.nav-link").click( function(ev) {
     ev.preventDefault();
-    console.log(ev);
+    var link = $(this);
+    $(document.body).animate({
+        opacity: '0'
+    }, 1000, function() {
+        document.location = link.attr("href");
+    });
 });
