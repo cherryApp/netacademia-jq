dd400206ff360f3e73831d5b1c835841f2402bee
cherryApp <j.cserko@gmail.com>
2017-10-05 18:46:45 +0200

    Feat: table filter.

--- js/main.js
+++ js/main.js
@@ -68,3 +68,90 @@ function showInvalidMessage() {
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+// Jegyek tömbje.
+var tickets = [
+  {
+    event: "Sziget Fesztivál",
+    time: "2018-08-03 18:00:00",
+    seller: "Kis Márton",
+    pcs: 5,
+    link: "licit/1"
+  },
+  {
+    event: "Diótörő Balett",
+    time: "2018-08-03 18:00:00",
+    seller: "Nagy Ádám",
+    pcs: 5,
+    link: "licit/1"
+  },
+  {
+    event: "MOMA Party",
+    time: "2018-08-03 18:00:00",
+    seller: "Brezeviczy Krisztián",
+    pcs: 5,
+    link: "licit/1"
+  },
+  {
+    event: "A Kékszakállú Herceg vára",
+    time: "2018-08-03 18:00:00",
+    seller: "Zwack Magdolna",
+    pcs: 5,
+    link: "licit/1"
+  },
+  {
+    event: "Balett mindenkinek",
+    time: "2018-08-03 18:00:00",
+    seller: "Schwarz Aurél",
+    pcs: 5,
+    link: "licit/1"
+  },
+  {
+    event: "Macskák Musical",
+    time: "2018-08-03 18:00:00",
+    seller: "Cserkó József",
+    pcs: 5,
+    link: "licit/1"
+  },
+];
+
+// Jegyek táblájának generálása.
+var ticketTable = $("table.table.table-striped").eq(0);
+function fillTicketsTable(currentTickets) {
+  currentTickets = currentTickets || tickets;
+  var tbody = ticketTable.find("tbody");
+  tbody.html("");
+  $.each( currentTickets, function(index, ticket) {
+    var row = $(".templates .ticket-row").clone();
+    row.find("td").eq(0).html(index+1);
+    row.find("td").eq(1).html(ticket.event);
+    row.find("td").eq(2).html(ticket.time);
+    row.find("td").eq(3).html(ticket.seller);
+    row.find("td").eq(4).html(ticket.pcs);
+    row.find("td").eq(5).html(ticket.link);
+    tbody.append(row);
+  });
+}
+fillTicketsTable();
+
+// Jegyek táblázat szűrése.
+$(".tickets-search-row input").on("keyup", filterTickets);
+function filterTickets() {
+  var currentValue = $(this).val().toLowerCase();
+  var filteredTickets = [];
+  if (currentValue == "") {
+    filteredTickets = tickets;
+  } else {
+    filteredTickets = tickets.filter( function(item) {
+      var done = false;
+      for (var k in item) {
+        if (item[k].toString().toLowerCase().indexOf(currentValue) > -1) {
+          done = true;
+        }
+      }
+      return done;
+    });
+  }
+
+  fillTicketsTable(filteredTickets);
+}
